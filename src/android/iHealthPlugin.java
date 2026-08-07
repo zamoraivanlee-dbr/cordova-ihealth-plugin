fbea484a3d4cccf784812ff6ced8bac5e5aac1a6
Ivan-Lee Zamora <ivan-lee.zamora@aptouring.com.au>
2026-08-07 14:26:43 +1000

    Include iHealth SDK and certificates

--- src/android/iHealthPlugin.java
+++ src/android/iHealthPlugin.java
@@ -1,27 +1,34 @@
 package cordova.ihealth.plugin;
 
-import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.CallbackContext;
-
 import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-/**
- * This class echoes a string called from JavaScript.
- */
 public class iHealthPlugin extends CordovaPlugin {
 
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if ("showToast".equals(action)) {
-            String message = args.getString(0);
-            int duration = args.getInt(1);
-            showToast(message, duration);
-            callbackContext.success();
-            return true;
+        switch(action) {
+            case "showToast":
+                String message = args.getString(0);
+                int duration = args.getInt(1);
+                showToast(message, duration);
+                callbackContext.success();
+                return true;
+            case "checkAuthorization":
+                boolean isAuthorized = checkAuthorization();
+                callbackContext.success(isAuthorized ? 1 : 0);
+                return true;
+            default:
+                return false;
         }
-        return false;
     }
 
     private void showToast(final String message, final int duration) {
@@ -30,5 +37,21 @@ public class iHealthPlugin extends CordovaPlugin {
                 Toast.makeText(cordova.getActivity(), message, duration).show();
             }
         });
+    }
+
+    private boolean checkAuthorization() {
+        boolean isAuthorized;
+
+        try (InputStream is = cordova.getActivity().getAssets().open("com_guardian_android_patient_dev_android.pem")) {
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            isAuthorized = iHealthDevicesManager.getInstance().sdkAuthWithLicense(buffer);
+        } catch (IOException e) {
+            e.printStackTrace();
+            isAuthorized = false;
+        }
+
+        return isAuthorized;
     }
 }
