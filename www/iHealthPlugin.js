d6fa00a2f8bcd2f83628388d8d38a9e69bdb8b0d
Ivan-Lee Zamora <ivan-lee.zamora@aptouring.com.au>
2026-08-07 15:34:03 +1000

    include gradle

--- www/iHealthPlugin.js
+++ www/iHealthPlugin.js
@@ -1,11 +1,92 @@
-var exec = require('cordova/exec');
+var exec = typeof require === 'function' ? require('cordova/exec') : null;
+
+function isCapacitorPluginAvailable() {
+    return typeof Capacitor !== 'undefined' && Capacitor.Plugins && Capacitor.Plugins.IHealthPlugin;
+}
+
+function isCordovaAvailable() {
+    return typeof cordova !== 'undefined' && exec;
+}
+
+function runCordovaAction(action, args, successCallback, errorCallback) {
+    if (!isCordovaAvailable()) {
+        var error = new Error('Cordova is not available');
+        if (errorCallback) {
+            errorCallback(error);
+            return;
+        }
+        return Promise.reject(error);
+    }
+
+    var callbackWrapper = function (result) {
+        if (typeof result === 'number') {
+            return result !== 0;
+        }
+        return result;
+    };
+
+    if (successCallback || errorCallback) {
+        exec(function (result) {
+            if (successCallback) {
+                successCallback(callbackWrapper(result));
+            }
+        }, errorCallback, 'iHealthPlugin', action, args);
+        return;
+    }
+
+    return new Promise(function (resolve, reject) {
+        exec(function (result) {
+            resolve(callbackWrapper(result));
+        }, reject, 'iHealthPlugin', action, args);
+    });
+}
+
+function normalizeResult(result) {
+    if (result && typeof result === 'object' && result.hasOwnProperty('authorized')) {
+        return result.authorized;
+    }
+    return result;
+}
+
+function runCapacitorAction(method, options, successCallback, errorCallback) {
+    if (!isCapacitorPluginAvailable()) {
+        var error = new Error('Capacitor plugin is not available');
+        if (errorCallback) {
+            errorCallback(error);
+            return;
+        }
+        return Promise.reject(error);
+    }
+
+    var promise = Capacitor.Plugins.IHealthPlugin[method](options || {});
+    if (successCallback || errorCallback) {
+        promise.then(function (result) {
+            if (successCallback) {
+                successCallback(normalizeResult(result));
+            }
+        }).catch(function (err) {
+            if (errorCallback) {
+                errorCallback(err);
+            }
+        });
+        return;
+    }
+
+    return promise.then(normalizeResult);
+}
 
 let iHealthPlugin = {
-    showToast: (message, duration, successCallback, errorCallback) => {
-        exec(successCallback, errorCallback, 'iHealthPlugin', 'showToast', [message, duration]);
+    showToast: function (message, duration, successCallback, errorCallback) {
+        if (isCapacitorPluginAvailable()) {
+            return runCapacitorAction('showToast', { message: message, duration: duration }, successCallback, errorCallback);
+        }
+        return runCordovaAction('showToast', [message, duration], successCallback, errorCallback);
     },
-    checkAuthorization: (successCallback, errorCallback) => {
-        exec(successCallback, errorCallback, 'iHealthPlugin', 'checkAuthorization', []);
+    checkAuthorization: function (successCallback, errorCallback) {
+        if (isCapacitorPluginAvailable()) {
+            return runCapacitorAction('checkAuthorization', null, successCallback, errorCallback);
+        }
+        return runCordovaAction('checkAuthorization', [], successCallback, errorCallback);
     }
 };
 
